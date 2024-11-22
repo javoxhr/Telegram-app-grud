eb1d6e2c7a8b53ceaa2fb1928900f933f140e5dd
javoxhr <javohiruzum@gmail.com>
2024-11-22 20:14:15 +0500

    new

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,30 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="flex flex-wrap justify-center gap-[25px]">
+  <div class="container">
+    <div class="crds-wrp flex flex-wrap justify-center gap-[25px]">
       <productCard v-for="item in 20" :key="item"/>
     </div>
   </div>
 </template>
 
 <style>
+html {
+  background: #fff;
+}
+.container {
+  width: 100%;
+  max-width: 1240px;
+  margin: 0 auto;
+  padding: 0 20px;
+  
+}
 
+@media screen and (max-width: 500px) {
+  .crds-wrp {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+  }
+}
 </style>
