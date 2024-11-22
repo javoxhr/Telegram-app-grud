2a60bfc487ea88cf3ece0af0638886354c710fc9
javoxhr <javohiruzum@gmail.com>
2024-11-22 17:17:22 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import productCard from "./components/productCard.vue";
+export default {
+  components: {
+    productCard
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-wrap justify-center gap-[25px]">
+      <productCard v-for="item in 20" :key="item"/>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/productCard.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <div class="product flex flex-col items-start w-44 rounded-xl p-4 relative" style="box-shadow: 1px 3px 7px 5px #ececec;">
+            <div class="image-wrap flex items-center justify-center w-full">
+                <img class="item-img w-24" src="../assets/images/png/burger.png" alt="">
+            </div>
+            <div class="text-wrapper flex flex-col gap-1 mt-2">
+                <h3 class="product-title text-[20px] font-medium">Product title</h3>
+                <span class="product-price">$ 60</span>
+            </div>
+            <button class="buy-now w-full py-2 rounded font-semibold bg-transparent border border-gray-500 text-gray-600 mt-4 cursor-pointer">Savatga qo'shish</button>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
